76fe3be074e34827a3f80e2bf3debcdcf5ff92a4
celil çavuş <celilcavus13@hotmail.com>
2023-03-03 21:50:38 +0300

    gerekli tüm güncellemeler yapıldı

--- app.js
+++ app.js
@@ -13,6 +13,22 @@ app.set('view engine','ejs');
 app.use(expres.static('public'));
 
 
+//admin panel - start
+app.get('/admin',(req,res)=>{
+    res.render('admin');
+});
+//admin panel - end
+app.get('/product-add',(req,res)=>{
+    res.render('./product/product-add');
+});
+
+
+
+
+
+
+
+
 
 
 app.get('/',(req,res)=>{

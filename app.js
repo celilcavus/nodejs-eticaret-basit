5e78468d6a4ac7c05fcd1a500e83687ad05fb575
celil çavuş <celilcavus13@hotmail.com>
2023-03-04 14:40:04 +0300

    category işlemleri tamamlandı

--- app.js
+++ app.js
@@ -18,52 +18,57 @@ const productionDetails = require('./controller/productDetails');
 const Product = require('./controller/product');
 // product end
 
-app.set('view engine','ejs');
+
+// Category start
+const Category = require('./controller/category');
+// Category end
+
+app.set('view engine', 'ejs');
 app.use(expres.static('public'));
 
 
 //admin panel - start
-app.get('/admin',(req,res)=>{
+app.get('/admin', (req, res) => {
     res.render('admin');
 });
 //admin panel - end
 
 //Product - start
 //#region ===================== PRODUCT AND PRODUC DETAILS
-app.get('/product-add',(req,res)=>{
+app.get('/product-add', (req, res) => {
     Product.getAll().then((result) => {
-        res.render('./product/product-add',{model:result[0]});
+        res.render('./product/product-add', { model: result[0] });
     }).catch((err) => {
         console.log(err);
     });
 });
 
-app.post('/product-add',(req,res)=>{
+app.post('/product-add', (req, res) => {
     const products = {
-        name:req.body.name,
-        price:req.body.price,
-        description:req.body.description,
-        categoryId:req.body.categoryId,
+        name: req.body.name,
+        price: req.body.price,
+        description: req.body.description,
+        categoryId: req.body.categoryId,
     };
     Product.Insert(
         products.name,
         products.price,
         products.description,
         products.categoryId
-        ).then((result) => {
-       if(result){
-           
+    ).then((result) => {
+        if (result) {
+
             res.redirect('/product-add');
-       }
-       
+        }
+
     }).catch((err) => {
         console.log(err);
     });
 });
 
-app.get('/product-delete/:id',(req,res)=>{
+app.get('/product-delete/:id', (req, res) => {
     Product.delete(req.params.id).then((result) => {
-        if(result){
+        if (result) {
             res.redirect('/product-add');
         }
     }).catch((err) => {
@@ -71,21 +76,21 @@ app.get('/product-delete/:id',(req,res)=>{
     });
 })
 
-app.get('/product-update/:id',(req,res)=>{
+app.get('/product-update/:id', (req, res) => {
     Product.getById(req.params.id).then((result) => {
-        res.render('./product/product-update',{model:result[0]});
+        res.render('./product/product-update', { model: result[0] });
     }).catch((err) => {
         console.log(err);
     });
 })
 
-app.post('/product-update',(req,res)=>{
+app.post('/product-update', (req, res) => {
     const products = {
-        Id:req.body.Id,
-        name:req.body.name,
-        price:req.body.price,
-        description:req.body.description,
-        categoryId:req.body.categoryId,
+        Id: req.body.Id,
+        name: req.body.name,
+        price: req.body.price,
+        description: req.body.description,
+        categoryId: req.body.categoryId,
     };
     Product.Update(
         products.name,
@@ -97,28 +102,28 @@ app.post('/product-update',(req,res)=>{
         if (result) {
             res.redirect('/product-add');
         }
-       
+
     }).catch((err) => {
         console.log(err);
     });
 });
 
 
-app.get('/product-addDetails/:id',(req,res)=>{
+app.get('/product-addDetails/:id', (req, res) => {
     productionDetails.getById(req.params.id).then((result) => {
-        res.render('./product/product-addDetails',{model:req.params.id});
+        res.render('./product/product-addDetails', { model: req.params.id });
     }).catch((err) => {
         console.log(err);
     });
-   
+
 });
 
-app.post('/product-addDetails',(req,res)=>{
+app.post('/product-addDetails', (req, res) => {
     const prodcutDetails = {
-        productId :req.body.productId,
-        productionCountry:req.body.productionCountry,
-        productionDate:req.body.productionDate,
-        longDescription:req.body.longDescription
+        productId: req.body.productId,
+        productionCountry: req.body.productionCountry,
+        productionDate: req.body.productionDate,
+        longDescription: req.body.longDescription
     }
     productionDetails.Insert(
         prodcutDetails.productId,
@@ -126,17 +131,17 @@ app.post('/product-addDetails',(req,res)=>{
         prodcutDetails.productionDate,
         prodcutDetails.longDescription)
         .then((result) => {
-        if (result) {
-            res.redirect('/product-add');
-        }
-    }).catch((err) => {
-        console.log(err);
-    });
+            if (result) {
+                res.redirect('/product-add');
+            }
+        }).catch((err) => {
+            console.log(err);
+        });
 });
 
-app.get('/product-viewDetails/:id',(req,res)=>{
+app.get('/product-viewDetails/:id', (req, res) => {
     productionDetails.getView(req.params.id).then((result) => {
-        res.render('./product/product-viewDetails',{model:result[0]});
+        res.render('./product/product-viewDetails', { model: result[0] });
     }).catch((err) => {
         console.log(err);
     });
@@ -146,8 +151,65 @@ app.get('/product-viewDetails/:id',(req,res)=>{
 
 
 //#region  ===================== CATEGORY =====================
+app.get('/category-add', (req, res) => {
+    Category.getAll().then((result) => {
+        res.render('./Category/category-add', { model: result[0] });
+    }).catch((err) => {
+        console.log(err);
+    });
+});
 
+app.post('/categoryadd', (req, res) => {
+    const cat = {
+        name: req.body.categoryname
+    };
 
+    Category.Insert(cat.name).then((result) => {
+        if (result) {
+            res.redirect('/category-add');
+        }
+        else {
+
+        }
+    }).catch((err) => {
+        console.log(err);
+    });
+});
+
+app.get('/category-delete/:id', (req, res) => {
+    Category.delete(req.params.id).then((result) => {
+        res.redirect('/category-add');
+    }).catch((err) => {
+        console.log(err);
+    });
+})
+
+app.get('/category-update/:id', (req, res) => {
+    Category.getById(req.params.id).then((result) => {
+        res.render('./Category/category-update', { model: result[0] });
+    }).catch((err) => {
+        console.log(err);
+    });
+});
+
+app.post('/category-update/', (req, res) => {
+    const categoryParams = {
+        Id: req.body.Id,
+        categoryname: req.body.categoryname
+    };
+
+    Category.Update
+        (
+            categoryParams.categoryname,
+            categoryParams.Id
+        ).then((result) => {
+
+            res.redirect('/category-add');
+
+        }).catch((err) => {
+            console.log(err);
+        });
+});
 
 //#endregion
 
@@ -155,11 +217,11 @@ app.get('/product-viewDetails/:id',(req,res)=>{
 
 
 //index(home)
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     prop.getAll().then((result) => {
-        res.render('index',{model:result[0]});
+        res.render('index', { model: result[0] });
     }).catch((err) => {
-       console.log(err); 
+        console.log(err);
     });
 });
 
@@ -168,6 +230,6 @@ app.get('/',(req,res)=>{
 
 
 
-app.listen(3000,()=>{
+app.listen(3000, () => {
     console.log("3000 portu dinlemede");
 });
--- controller/category.js
+++ controller/category.js
@@ -7,11 +7,11 @@ class Category {
     static getById(id){
         return context.execute(`SELECT  * FROM ${tablename.toString()} where Id = ?`,[id]);
     }
-    static Insert(name){
-        return context.execute(`INSERT INTO ${tablename.toString()} (name) values (?)`,[name]);
+    static Insert(categoryname){
+        return context.execute(`INSERT INTO ${tablename.toString()} (categoryname) values (?)`,[categoryname]);
     }
     static Update(name,id){
-        return context.execute(`UPDATE ${tablename.toString()} SET name = ? WHERE Id = ?`,[name,id]);
+        return context.execute(`UPDATE ${tablename.toString()} SET categoryname = ? WHERE Id = ?`,[name,id]);
     }
     static delete(id){
         return context.execute(`DELETE FROM ${tablename.toString()} WHERE Id = ?`,[id]);
